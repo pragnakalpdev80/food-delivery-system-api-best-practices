7e07ab9ce46e0b23793dd2ed4224f9d78ba7cb82
Hill Soni Dev80 <pragnakalp.dev80@gmail.com>
2026-04-14 17:01:06 +0530

    [UPDATED] Selectors and Settings

--- config/settings.py
+++ config/settings.py
@@ -171,7 +171,7 @@ REST_FRAMEWORK = {
     },
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
     'DEFAULT_VERSION': 'v1',
-    'ALLOWED_VERSIONS': ['v1', 'v2'],
+    'ALLOWED_VERSIONS': ['v1'],
     'VERSION_PARAM': 'version',
 
     'DEFAULT_FILTER_BACKENDS': [
